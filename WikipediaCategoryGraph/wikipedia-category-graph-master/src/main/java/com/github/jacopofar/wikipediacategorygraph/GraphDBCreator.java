bd706b19e6e62ce2dcf82c74af3b504d6ea9d725
IncioMan <incio_man@live.it>
2018-11-20 15:01:09 +0100

    fix

--- WikipediaCategoryGraph/wikipedia-category-graph-master/src/main/java/com/github/jacopofar/wikipediacategorygraph/GraphDBCreator.java
+++ WikipediaCategoryGraph/wikipedia-category-graph-master/src/main/java/com/github/jacopofar/wikipediacategorygraph/GraphDBCreator.java
@@ -7,7 +7,7 @@ import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 public class GraphDBCreator {
 	public static GraphDatabaseService getGraphDatabase(String filePath) {
-		File db = new File("C:\\Users\\Alex\\Desktop\\testdb.db");
+		File db = new File(filePath);
 		return new GraphDatabaseFactory().newEmbeddedDatabase(db);
 	}
 }

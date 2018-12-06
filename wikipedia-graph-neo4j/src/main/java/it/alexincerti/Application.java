24c2c09349a97522bdd2d64dab79f79198c5ab5b
IncioMan <incio_man@live.it>
2018-12-06 17:40:27 +0100

    refactors

--- wikipedia-graph-neo4j/src/main/java/it/alexincerti/Application.java
+++ wikipedia-graph-neo4j/src/main/java/it/alexincerti/Application.java
@@ -8,19 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-//		WikiCategoryGraphDBCreator dbCreator = SpringApplication.run(Application.class, args)
-//				.getBean(WikiCategoryGraphDBCreator.class);
-//		dbCreator.create();
-//		CalculateClosestMainCategory calculateClosestMainCategory =
 		SpringApplication.run(Application.class, args);
-		// .getBean(CalculateClosestMainCategory.class);
-//		calculateClosestMainCategory.calculateShortestDistancesToMainCategories(
-//				args[0],
-//				args[1], args[2],Integer.parseInt(args[3]), Long.parseLong(args[4]));
-
-//		calculateClosestMainCategory.calculateShortestDistancesToMainCategories(
-//				"C:\\Users\\Alex\\Documents\\category.sql",
-//				"Arts|Concepts|Culture|Education|Events|Geography|Health|History|Humanities|Language|Law|Life|Mathematics|Nature|People|Philosophy|Politics|Reference|Religion|Science|Society|Sports|Technology|Universe|World",
-//				"output.txt", 1, 20l);
 	}
 }
